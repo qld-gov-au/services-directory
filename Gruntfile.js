03ac2741528828a7960971e02a2420911bd0c7fe
Kylie Rose <thisiskylierose@gmail.com>
2014-12-02 11:32:46 +1000

    added outages ssi

--- Gruntfile.js
+++ Gruntfile.js
@@ -478,7 +478,7 @@ module.exports = function (grunt) {
         }
 
         grunt.task.run([
-            'development',
+            'dev',
             'connect:livereload',
             'watch'
         ]);

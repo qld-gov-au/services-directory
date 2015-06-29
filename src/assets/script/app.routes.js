c319911805c86c6dd222a08ec94516fda0de7d38
William Savage <wsavage@gmail.com>
2015-06-29 18:03:31 +1000

    commented out testing resources

--- src/assets/script/app.routes.js
+++ src/assets/script/app.routes.js
@@ -29,8 +29,8 @@ qg.swe.services = (function ( $, swe ) {
             slug: 'transport-and-motoring'
         },
         resource: {
-//            id: 'c361766e-f9d4-490a-817d-5effbdd97ba5',
-//            url: 'staging.data.qld.gov.au'
+        //  id: 'c361766e-f9d4-490a-817d-5effbdd97ba5',
+        //  url: 'staging.data.qld.gov.au'
             id: '384429ae-fd27-4448-afe6-e4ecb8d1ad93',
             url: 'data.qld.gov.au'
         }
